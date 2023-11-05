dc0e216b36c176752e0e667dcb147ad444953377
schwepish <felix.kahuta@student.moringaschool.com>
2023-11-05 11:39:37 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+const projectItem = projects.map((project) => (
+  <ProjectItem 
+  key={project.id}
+  name={project.name}
+  about={project.about}
+  technologies={project.technologies}
+  />
+));
+Steve Kariuki
+11:38 AM
 import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+
+function NavBar() {
+return (
+  <nav>
+    <a href="#home">I'm a link!</a>
+  </nav>
+);
+}
+
+function Home() {
+return (
+  <div id="home">
+    <h1>Home</h1>
+  </div>
+);
+}
+
+function About() {
+return (
+  <div id="about">
+    <h2>About</h2>
+  </div>
+);
+}
 
 function App() {
-  return (
-    <div>
-      <Article />
-      <Comment />
-    </div>
-  );
+return (
+  <div>
+    <NavBar />
+    <Home />
+    <About />
+  </div>
+);
 }
 
 export default App;
